929553dafbc0a762c54117e4ec16e3a056d1482b
vanazco <vanazco@elpuig.xeill.net>
2018-11-08 18:49:44 +0100

    a

---
+++ src/com/company/Alimentacio.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Alimentacio extends Producte {
+
+    //Precio alimentacion no se guarda
+
+    //get preu ( super )
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int op,OP;
+        System.out.println("BENVINGUT AL ");
+
+    }
+}
