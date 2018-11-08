8d7c3f60f6cc46e25ffbdf284236ef55e1fe4604
vanazco <vanazco@elpuig.xeill.net>
2018-11-08 19:03:10 +0100

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int op,OP;
-        System.out.println("BENVINGUT AL ");
+        System.out.println("BENVINGUT AL " );
 
     }
 }
