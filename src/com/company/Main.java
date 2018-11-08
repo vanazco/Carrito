ff174a452a8dca4fc3dcf8420e0bf73f28530429
vanazco <vanazco@elpuig.xeill.net>
2018-11-08 20:05:42 +0100

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,65 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static int menuPrincipal(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1- Añadir Producto");
+        System.out.println("2- Pasar por caja");
+        System.out.println("3- Mostrar Carrito");
+        System.out.println("0- Salir");
+        System.out.print("Que quieres hacer?: ");
+        int op = sc.nextInt();
+        sc.nextLine();
+        return op;
+    }
+
+    static int menuProducte(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1- Añadir Alimento");
+        System.out.println("2- Añadir Electronico");
+        System.out.println("3- Añadir Textil");
+        System.out.println("0- Salir");
+        System.out.print("Que quieres hacer?: ");
+        int OP = sc.nextInt();
+        sc.nextLine();
+        return OP;
+    }
 
     public static void main(String[] args) {
 
-        int op,OP;
-        System.out.println("BENVINGUT AL " );
+        final String NOM_SUPERMERCAT = "NdeNayan";
 
+
+        int op,OP;
+        System.out.println("BENVINGUT AL " + NOM_SUPERMERCAT);
+        do {
+            op = menuPrincipal();
+            switch(op) {
+                case 1:
+                    do {
+                        OP = menuProducte();
+                        switch (OP) {
+                            case 1:
+                                System.out.println("Afegir Aliment"); break;
+                            case 2:
+                                System.out.println("Afegir electronic"); break;
+                            case 3:
+                                System.out.println("Afegir Textil"); break;
+                                default: break;
+                        }
+                    } while (OP!=0);
+                    break;
+                case 2:
+                    System.out.println("Pasar por caja");break;
+                case 3:
+                    System.out.println("Carrito");break;
+                case 0:
+                    System.out.println("Gracias por tu visita");break;
+                default:break;
+            }
+        }while (op!= 0);
     }
 }
--- src/com/company/Producte.java
+++ src/com/company/Producte.java
@@ -2,5 +2,32 @@ package com.company;
 
 public class Producte {
 
-    //Todo private
+    private float preu;
+    private String nom;
+    private int codiBarres;
+
+    public float getPreu() {
+        return preu;
+    }
+
+    public void setPreu(float preu) {
+        this.preu = preu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCodiBarres() {
+        return codiBarres;
+    }
+
+    public void setCodiBarres(int codiBarres) {
+        this.codiBarres = codiBarres;
+    }
+
 }
